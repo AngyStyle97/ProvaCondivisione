0959c5eff21faab5450a515dbef9efdbd3a6491a
Angelo De Fenza <angystyle97@outlook.it>
2026-06-28 12:17:42 +0200

    Buonanotte

---
+++ src/entity/Persona.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class Persona {
+      private String name;
+      private String surname;
+      
+      public Persona(String name, String surname) {
+    	  this.name = name;
+    	  this.surname = surname;
+      }
+      
+}
